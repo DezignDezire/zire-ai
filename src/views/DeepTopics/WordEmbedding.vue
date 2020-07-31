f842f2ccb3328106c832f7809764cb3dff95ae3a
Philipp Lenz <philipp.lenz@hotmail.com>
2020-07-31 11:20:34 +0200

    margins updated for mobile

--- src/views/DeepTopics/WordEmbedding.vue
+++ src/views/DeepTopics/WordEmbedding.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="text">
+    Neural networks cannot simply process words or images.
+    They need a digit-vector representation of the input (e.g. obtained through word embedding). <br>
     <h2>Word Embedding</h2>
-    Building a low-dimensional vector representation from a corpus of text, which preserves the contextual similarity of words.<br>
-    Neural networks cannot simply process words or images. They need a digit-vector representation of the input (e.g. obtained through word embedding).
-    <br>
-    <img src="./word2vec.png" class="img">
+    Words that appear next to each other (in sentences) are assigned a similar position in the vector space (e.g. the dog eats dogwood). <br>
+    The multi-dimensional representation allows one to measure the cosine-distance between words and therefore how “contextually similar” these words are.
+    Given a certain word, its nearest-neighbour can easily be found. <br>
     <br>
     <h3>Skip-Gram Model</h3>
+    <img src="./word2vec.png" class="img">
+    <br>
     It aims to predict the probability of context words given an input word. <br>
-    By sliding a window over each word of a sentence, tuples of word and their neighboring words are created. (dog, eats) <br>
-    The model consists of an input and output vector with size of vocabulary and hidden vector with size of the models dimensions. <br>
-    Input containing first word of tuple as one-hot encoded word representations and output containing second word respectively. <br>
+    By sliding a window over each word of a sentence, tuples of word and their neighboring words are created (dog, eats). <br>
+    The model consists of an input and output vector with size of vocabulary and hidden vector with size of the models dimensions.
+    Input containing first word of tuple as one-hot encoded word representations and output containing second word respectively.
     Throughout training process the weights of the matrixes W and W’ are trained using backpropagation. <br>
     <br>
     An arising problem is that the correct meaning of a word is defined by its surrounding context (bat ≠ bat).
@@ -26,11 +29,33 @@ export default {
 
 <style>
 .img{
-    width: 500px;
-    margin: 40px 0;
+    width: 600px;
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    margin-bottom: 30px;
 }
 
 .text{
-    margin-bottom: 200px;
+    width: 800px;
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    text-align: left;
+    margin-bottom: 5rem;
 }
+
+@media (max-width: 800px) {
+    .text{
+        width: auto;
+        margin: 1rem 1rem 5rem 1rem;
+    }
+}
+
+@media (max-width: 600px) {
+    .img{
+      width: 100%;
+    }
+}
+
 </style>
