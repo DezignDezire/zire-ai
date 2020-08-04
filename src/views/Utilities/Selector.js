1f74c3836aee73fcc3ecc96fc677f32ebacf00dd
Philipp Lenz <philipp.lenz@hotmail.com>
2020-08-04 12:03:15 +0200

    scroll updated

--- src/views/Utilities/Selector.js
+++ src/views/Utilities/Selector.js
@@ -2,10 +2,9 @@ import store from '../../store'
 
 export default class Selector {
   navigateToTopic (topicTitle) {
-    console.log('topic-' + topicTitle)
     document
       .getElementById('item-' + topicTitle)
-      .scrollIntoView({ behavior: 'smooth', block: 'center' })
+      .scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'center' })
     store.dispatch('selectTopic', topicTitle)
   }
 }
